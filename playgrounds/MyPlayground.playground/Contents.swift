09ef666dc2cff77e86e307d8784503046943dfe9
CrowdedAstronaut <ross@johnrossphillips.com>
2022-02-21 09:51:59 -0600

    Completed FizzBuzz Swift style

--- playgrounds/MyPlayground.playground/Contents.swift
+++ playgrounds/MyPlayground.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//FizzBuzz 1
+	
+for i in 1...100 {
+	if i.isMultiple(of: 3) {
+		if i.isMultiple(of: 5) {
+			print("FizzBuzz")
+		} else {
+			print("Fizz")
+		}
+	}else if i.isMultiple(of: 5){
+		print("Buzz")
+	} else {
+		print(i)
+	}
+}
