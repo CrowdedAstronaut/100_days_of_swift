659e42126de055ed16faa81063bafc99e398f3e5
CrowdedAstronaut <ross@johnrossphillips.com>
2022-02-21 12:05:31 -0600

    Conditionals and loops summary

--- playgrounds/MyPlayground.playground/Contents.swift
+++ playgrounds/MyPlayground.playground/Contents.swift
@@ -61,3 +61,18 @@ var greeting = "Hello, playground"
 //Enums let us create our own simple types in Swift so that we can specify a range of acceptable values such as a list of actions the user can perform, the types of files we are able to write, or the types of notification to send to the user.
 //Swift must always know the type of data inside a constant or variable, and mostly uses type inference to figure that out based on the data we assign. However, it’s also possible to use type annotation to force a particular type.
 //Out of arrays, dictionaries, and sets, it’s safe to say that you’ll use arrays by far the most. After that comes dictionaries, and sets come a distant third. That doesn’t mean sets aren’t useful, but you’ll know when you need them!
+
+
+//Conditionals and Loops
+//We’ve covered a lot about conditions and loops in the previous chapters, so let’s recap:
+//
+//We use if statements to check a condition is true. You can pass in any condition you want, but ultimately it must boil down to a Boolean.
+//If you want, you can add an else block, and/or multiple else if blocks to check other conditions. Swift executes these in order.
+//You can combine conditions using ||, which means that the whole condition is true if either subcondition is true, or &&, which means the whole condition is true if both subconditions are true.
+//If you’re repeating the same kinds of check a lot, you can use a switch statement instead. These must always be exhaustive, which might mean adding a default case.
+//If one of your switch cases uses fallthrough, it means Swift will execute the following case afterwards. This is not used commonly.
+//The ternary conditional operator lets us check WTF: What, True, False. Although it’s a little hard to read at first, you’ll see this used a lot in SwiftUI.
+//for loops let us loop over arrays, sets, dictionaries, and ranges. You can assign items to a loop variable and use it inside the loop, or you can use underscore, _, to ignore the loop variable.
+//while loops let us craft custom loops that will continue running until a condition becomes false.
+//We can skip some or all loop items using continue or break respectively.
+//That’s another huge chunk of new material, but with conditions and loops you now know enough to build some really useful software – give it a try!
