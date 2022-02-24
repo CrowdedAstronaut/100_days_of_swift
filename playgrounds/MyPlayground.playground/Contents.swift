10768cd8e3937598e814c36aedc2ec473c0591d7
CrowdedAstronaut <ross@johnrossphillips.com>
2022-02-24 09:52:45 -0600

    completed property observer lesson

--- playgrounds/MyPlayground.playground/Contents.swift
+++ playgrounds/MyPlayground.playground/Contents.swift
@@ -257,3 +257,23 @@ var greeting = "Hello, playground"
 //print(archer.vacationRemaining)
 //archer.vacationRemaining = 10
 //print(archer.vacationAllocated)
+
+
+//struct App {
+//	var contacts = [String]() {
+//		willSet {
+//			print("Current value is: \(contacts)")
+//			print("New value will be: \(newValue)")
+//		}
+//
+//		didSet {
+//			print("There are now \(contacts.count) contacts.")
+//			print("Old value was \(oldValue)")
+//		}
+//	}
+//}
+//
+//var app = App()
+//app.contacts.append("Adrian E")
+//app.contacts.append("Allen W")
+//app.contacts.append("Ish S")
