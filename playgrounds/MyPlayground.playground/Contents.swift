39ff0d42ce98a1243bac916fdf572ef289b4172d
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-01 12:47:49 -0600

    Completed lessons on extensions and protocol

--- playgrounds/MyPlayground.playground/Contents.swift
+++ playgrounds/MyPlayground.playground/Contents.swift
@@ -712,3 +712,47 @@ var greeting = "Hello, playground"
 //		self.readingHours = pageCount / 50
 //	}
 //}
+
+//
+//extension Array {
+//	var isNotEmpty: Bool {
+//		isEmpty == false
+//	}
+//}
+//
+//
+//let guests = ["Mario", "Luigi", "Peach"]
+//
+//if guests.isNotEmpty {
+//	print("Guest count: \(guests.count)")
+//}
+//
+//protocol Person {
+//	var name: String { get }
+//	func sayHello()
+//}
+//
+//extension Person {
+//	func sayHello() {
+//		print("Hi, I'm \(name)")
+//	}
+//}
+//
+//struct Employee: Person {
+//	let name: String
+//}
+//
+//let taylor = Employee(name: "Taylor Swift")
+//taylor.sayHello()
+//
+//
+//let numbers = [4, 8, 15, 16]
+//let allEven = numbers.allSatisfy { $0.isMultiple(of: 2) }
+//
+//
+//let numbers2 = Set([4, 8, 15, 16])
+//let allEven2 = numbers2.allSatisfy { $0.isMultiple(of: 2) }
+//
+//
+//let numbers3 = ["four": 4, "eight": 8, "fifteen": 15, "sixteen": 16]
+//let allEven3 = numbers3.allSatisfy { $0.value.isMultiple(of: 2) }
