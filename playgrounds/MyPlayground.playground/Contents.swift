4c0b9f2537b12c4de7cb87c3816231fa4aec16fd
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-01 11:12:35 -0600

    Completed lesson on protocol.

--- playgrounds/MyPlayground.playground/Contents.swift
+++ playgrounds/MyPlayground.playground/Contents.swift
@@ -588,3 +588,65 @@ var greeting = "Hello, playground"
 //		print("meow")
 //	}
 //}
+
+//
+//func commute(distance: Int, using vehicle: Car) {
+//	// lots of code here
+//}
+//
+//func commute(distance: Int, using vehicle: Train) {
+//	// lots of code here
+//}
+//
+//protocol Vehicle {
+//	func estimateTime(for distance: Int) -> Int
+//	func travel(distance: Int)
+//	
+//}
+//
+//struct Car: Vehicle {
+//	func estimateTime(for distance: Int) -> Int {
+//		distance / 50
+//	}
+//	
+//	func travel(distance: Int) {
+//		print("I'm driving \(distance)km.")
+//	}
+//	
+//	func openSunroof() {
+//		print("It's a nice day!")
+//	}
+//}
+//
+//struct Bicycle: Vehicle {
+//	func estimateTime(for distance: Int) -> Int {
+//		distance / 10
+//	}
+//
+//	func travel(distance: Int) {
+//		print("I'm cycling \(distance)km.")
+//	}
+//}
+//
+//let bike = Bicycle()
+//commute(distance: 50, using: bike)
+//
+//
+//
+//func commute(distance: Int, using vehicle: Vehicle) {
+//	if vehicle.estimateTime(for: distance) > 100 {
+//		print("That's too slow! I'll try a different vehicle.")
+//	} else {
+//		vehicle.travel(distance: distance)
+//	}
+//}
+//
+//let car = Car()
+//commute(distance: 100, using: car)
+
+protocol Vehicle {
+	var name: String { get }
+	var currentPassengers: Int { get set }
+	func estimateTime(for distance: Int) -> Int
+	func travel(distance: Int)
+}
