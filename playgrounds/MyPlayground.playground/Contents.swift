518f4ff53b84c3ac6a5a382d5b915cdc4c120d34
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-03 11:04:21 -0600

    Completed lesson on optionals.

--- playgrounds/MyPlayground.playground/Contents.swift
+++ playgrounds/MyPlayground.playground/Contents.swift
@@ -906,3 +906,36 @@ var greeting = "Hello, playground"
 //		Direction.allCases.randomElement()!
 //	}
 //}
+
+//
+//let captains = [
+//	"Enterprise": "Picard",
+//	"Voyager": "Janeway",
+//	"Defiant": "Sisko"
+//]
+//
+//let new = captains["Serenity"] ?? "N/A"
+//
+//let newCaptain = captains["Serenity", default: "N/A"]
+//
+//
+//let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+//let favorite = tvShows.randomElement() ?? "None"
+//
+//
+//struct Book {
+//	let title: String
+//	let author: String?
+//}
+//
+//let book = Book(title: "Beowulf", author: nil)
+//let author = book.author ?? "Anonymous"
+//print(author)
+//
+//let input = ""
+//let number = Int(input) ?? 0
+//print(number)
+//
+//
+//let scores = ["Picard": 800, "Data": 7000, "Troi": 900]
+//let crusherScore = scores["Crusher"] ?? 0
