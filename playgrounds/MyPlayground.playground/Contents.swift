fd0cd01fa9c20f1cd0ca4f3dc781a1beb57e4d44
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-03 11:40:27 -0600

    Completed checkpoint 9

--- playgrounds/MyPlayground.playground/Contents.swift
+++ playgrounds/MyPlayground.playground/Contents.swift
@@ -965,3 +965,50 @@ var greeting = "Hello, playground"
 //
 //let names2 = ["Vincent": "van Gogh", "Pablo": "Picasso", "Claude": "Monet"]
 //let surnameLetter = names2["Vincent"]?.first?.uppercased()
+
+//
+//enum UserError: Error {
+//	case badID, networkFailed
+//}
+//
+//func getUser(id: Int) throws -> String {
+//	throw UserError.networkFailed
+//}
+//
+//if let user = try? getUser(id: 23) {
+//	print("User: \(user)")
+//}
+//
+//let user = (try? getUser(id: 23)) ?? "Anonymous"
+//print(user)
+
+
+//You’ll find try? is mainly used in three places:
+//
+//In combination with guard let to exit the current function if the try? call returns nil.
+//In combination with nil coalescing to attempt something or provide a default value on failure.
+//When calling any throwing function without a return value, when you genuinely don’t care if it succeeded or not – maybe you’re writing to a log file or sending analytics to a server, for example.
+//
+//if let result = try? runRiskyFunction() {
+//	print(result)
+//}
+
+//In these chapters we’ve covered one of Swift’s most important features, and although most people find optionals hard to understand at first almost everyone agrees they are useful in practice.
+//
+//Let’s recap what we learned:
+//
+//Optionals let us represent the absence of data, which means we’re able to say “this integer has no value” – that’s different from a fixed number such as 0.
+//As a result, everything that isn’t optional definitely has a value inside, even if that’s just an empty string.
+//Unwrapping an optional is the process of looking inside a box to see what it contains: if there’s a value inside it’s sent back for use, otherwise there will be nil inside.
+//We can use if let to run some code if the optional has a value, or guard let to run some code if the optional doesn’t have a value – but with guard we must always exit the function afterwards.
+//The nil coalescing operator, ??, unwraps and returns an optional’s value, or uses a default value instead.
+//Optional chaining lets us read an optional inside another optional with a convenient syntax.
+//If a function might throw errors, you can convert it into an optional using try? – you’ll either get back the function’s return value, or nil if an error is thrown.
+//Optionals are second only to closures when it comes to language features folks struggle to learn, but I promise after a few months you’ll wonder how you could live without them!
+
+
+//Checkpoint 9
+
+func getNumber(in array: [Int]?) -> Int {
+	(array?.randomElement() ?? Int.random(in: 1...100))
+}
